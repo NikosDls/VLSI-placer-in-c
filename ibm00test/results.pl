1c49b139897ca317ae724c455edbbd9fa0dcfb7e
NikosDls <38583405+NikosDls@users.noreply.github.com>
2019-02-01 17:42:13 +0200

    Add files via upload

---
+++ ibm00test/results.pl
@@ -0,0 +1,11 @@
+# ISPD VLSI PLACEMENT BENCHMARK: ibm00test
+# PLACMENT WITH RANDOM GP AND TETRIS LG ALGORITHM
+# RANDOM GP RUNTIME       : 0.000000 seconds
+# TETRIS LG RUNTIME       : 0.016000 seconds
+# TOTAL WIRELENGTH (HPWL) : 738
+
+     a0          6        416
+     a1          0        416
+     p1          0          0 / FIXED
+     p2          1          1 / FIXED
+     p3          2          3 / FIXED
