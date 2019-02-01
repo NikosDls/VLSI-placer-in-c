2731146dd9f8b0b42cd69f59d231b9f64f92e366
NikosDls <38583405+NikosDls@users.noreply.github.com>
2019-02-01 17:40:58 +0200

    Add files via upload

---
+++ ibm00test/ibm00test.pl
@@ -0,0 +1,11 @@
+UCLA pl 1.0
+# Created     : Wed Oct  1 23:12:57 CDT 2003
+# User        : nataraj@saturn.ee.iastate.edu (Natarajan Viswanathan)
+# Platform    : SunOS 5.8 sparc SUNW,Sun-Fire-880
+
+
+p1          0        0 : FS 
+p2          1        0.5 : FS 
+p3          2        3 : FS 
+a0         0        0 : N 
+a1         0        0 : N 
